d6980c2deb8d06d3eae7b386af582ca4b7e03e7e
Rafal F <rafalfuchs2004@gmail.com>
2025-04-12 16:34:19 +0200

    Engine setup done

--- RGame/RGame.cpp
+++ RGame/RGame.cpp
@@ -1,36 +1,52 @@
 ﻿#include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3_Image/SDL_image.h>
 #include <spdlog/spdlog.h>
-#include <KitsuEngine/Test.hpp>
+#include <KitsuEngine/KitsuneEngine.hpp>
+
+#include <cassert>
+
+
+class mScene : public Scene
+{
+public:
+    mScene(Game* game) : Scene(game)
+    {
+
+    }
+    virtual void Init()
+    {
+
+    }
+    virtual void Update()
+    {
+
+    }
+    virtual void Draw()
+    {
+
+    }
+    virtual void Events(const SDL_Event& event)
+    {
+
+    }
+    ~mScene() 
+    {
+
+    }
+};
 
 int main(int argc, char** argv)
 {
-    spdlog::info("Starting SDL3...");
+    assert(KitsuEngineInit(800,600) == 0);
 
-    if (SDL_Init(SDL_INIT_VIDEO) != true) 
-    {
-        spdlog::error("SDL_Init failed: {}", SDL_GetError());
-        return -1;
-    }
+    Game* game = new Game();
+    mScene* mainScene = new mScene(game);
+    game->StartGame(mainScene);
+    
 
-    spdlog::info("SDL3 Initialized!");
-
-    // Inicjalizacja okna SDL
-    SDL_Window* window = SDL_CreateWindow("SDL3 Test", 800, 600, 0);
-    if (!window) 
-    {
-        spdlog::error("Failed to create window: {}", SDL_GetError());
-        SDL_Quit();
-        return -1;
-    }
-
-    spdlog::info("Window created!");
-
-
-    ThisIsATest();
-
-
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    delete game;
+    KitsuEngineClean();
+    
     return 0;
 }
