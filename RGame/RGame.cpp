fb7bcacacf5b67e25786b90544201a7e392e5b34
Rafal F <rafalfuchs2004@gmail.com>
2025-09-16 10:57:26 +0200

    Dekstop config handled

--- RGame/RGame.cpp
+++ RGame/RGame.cpp
@@ -1,7 +1,7 @@
 ﻿#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL_main.h>
 
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
 #include <SDL3_Image/SDL_image.h>
 #include <spdlog/spdlog.h>
 #include <KitsuEngine/KitsuneEngine.hpp>
@@ -457,7 +457,10 @@ SDL_AppResult SDL_AppIterate(void* appstate)
 
 SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 {
-    game->HandleEvents(event);
+    bool quit = game->HandleEvents(event);
+    if (quit)
+        return SDL_APP_SUCCESS;
+
     return SDL_APP_CONTINUE;
 }
 
