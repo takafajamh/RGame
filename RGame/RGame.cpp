6ce70bba4c379c2fe1e012bd08f1054ea7f387b2
Rafal F <rafalfuchs2004@gmail.com>
2026-01-30 18:25:01 +0100

    Callback fixes

--- RGame/RGame.cpp
+++ RGame/RGame.cpp
@@ -1,6 +1,8 @@
 ﻿#define SDL_MAIN_USE_CALLBACKS
-#include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+
+
+#include <SDL3/SDL.h>
 #include <SDL3_Image/SDL_image.h>
 #include <spdlog/spdlog.h>
 #include <KitsuEngine/KitsuneEngine.hpp>
